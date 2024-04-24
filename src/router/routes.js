716b4013982bea02d6923d49fe4f71c23af77759
Nitesh Yadav <nitesh.y@zifornd.com>
2024-04-24 17:59:34 +0530

    minor fix

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,3 @@
 import HomeView from "../views/HomeView.vue";
 
-export const routes = [{ path: "/", component: HomeView }];
+export const routes = [{ path: "/", component: HomeView, name: "home" }];
