e7b653c0796dfe286c663910c52938513bbfac03
Nitesh Yadav <nitesh.y@zifornd.com>
2024-04-25 00:36:05 +0530

    add checkAuth route guard

---
+++ src/router/guards.js
@@ -0,0 +1,14 @@
+import { useAuthStore } from "../stores/auth.js";
+import storage from "../utils/storage.js";
+import { STORAGE_TOKEN_KEY } from "../utils/constants.js";
+
+export async function checkAuth(to, from) {
+  const auth = useAuthStore();
+  if (!auth.isAuthenticated) {
+    if (storage.has(STORAGE_TOKEN_KEY)) {
+      await auth.restoreSession();
+      return true;
+    }
+    if (to.name !== "login") return { name: "login" };
+  }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { checkAuth } from "./guards.js";
 import { routes } from "./routes";
 
 export const router = createRouter({
   history: createWebHistory("/ui"),
   routes: routes,
 });
+router.beforeEach(checkAuth);
