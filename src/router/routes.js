56e281aa8eca6452b3486d74b8c171acb5f34c9b
Nitesh Yadav <nitesh.y@zifornd.com>
2024-04-25 14:24:26 +0530

    add src/view/chat/ChatView

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,9 @@
+import DialogView from "../views/dialog/DialogView.vue";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/auth/LoginView.vue";
 
 export const routes = [
   { path: "/", component: HomeView, name: "home" },
   { path: "/login", component: LoginView, name: "login" },
+  { path: "/dialog/:dialogId", component: DialogView, name: "dialog" },
 ];
