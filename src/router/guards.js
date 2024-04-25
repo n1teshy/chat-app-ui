8e080315cf1b17bb89707890ef427eb7282547dc
Nitesh Yadav <nitesh.y@zifornd.com>
2024-04-25 23:25:48 +0530

    minor change to checkAuth route guard

--- src/router/guards.js
+++ src/router/guards.js
@@ -2,7 +2,7 @@ import { useAuthStore } from "../stores/auth.js";
 import storage from "../utils/storage.js";
 import { STORAGE_TOKEN_KEY } from "../utils/constants.js";
 
-export async function checkAuth(to, from) {
+export async function checkAuth(to) {
   const auth = useAuthStore();
   if (!auth.isAuthenticated) {
     if (storage.has(STORAGE_TOKEN_KEY)) {
