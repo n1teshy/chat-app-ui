948217e400931a4b893793d7fcfa04f7ca038ec1
Nitesh Yadav <nitesh.y@zifornd.com>
2024-04-26 12:01:03 +0530

    logout user if they misbehave

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,6 +1,8 @@
 import baseAxios from "axios";
+import { useRouter } from "vue-router";
 import { useAuthStore } from "../stores/auth.js";
 
+const router = useRouter();
 export const axios = baseAxios.create({
   baseURL: "http://localhost:9000",
 });
@@ -16,11 +18,30 @@ axios.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
+const [
+  STATUS_BAD_REQUEST,
+  STATUS_FORBIDDEN,
+  STATUS_UNAUTHORIZED,
+  STATUS_NOT_FOUND,
+] = [400, 403, 401, 404];
+
 axios.interceptors.response.use(
   (response) => response.data,
   (error) => {
-    if (error.response && error.response.status === 422) {
+    const auth = useAuthStore();
+    const status = error.response.status;
+    if (error.response && status === 422) {
       return Promise.reject(error.response.data);
+    }
+    if (
+      status === STATUS_BAD_REQUEST ||
+      status === STATUS_FORBIDDEN ||
+      status === STATUS_UNAUTHORIZED ||
+      status === STATUS_NOT_FOUND
+    ) {
+      auth.clearSession();
+      router.push({ name: "login" });
+      return;
     }
     return Promise.reject({
       message: "something went wrong, please try again later.",
