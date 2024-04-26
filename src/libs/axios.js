2e789403d09681db853ea89cd519de58d914a001
Nitesh Yadav <nitesh.y@zifornd.com>
2024-04-26 12:22:08 +0530

    log users out if they misbehave

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -2,7 +2,6 @@ import baseAxios from "axios";
 import { useRouter } from "vue-router";
 import { useAuthStore } from "../stores/auth.js";
 
-const router = useRouter();
 export const axios = baseAxios.create({
   baseURL: "http://localhost:9000",
 });
@@ -28,23 +27,26 @@ const [
 axios.interceptors.response.use(
   (response) => response.data,
   (error) => {
-    const auth = useAuthStore();
-    const status = error.response.status;
-    if (error.response && status === 422) {
-      return Promise.reject(error.response.data);
-    }
-    if (
-      status === STATUS_BAD_REQUEST ||
-      status === STATUS_FORBIDDEN ||
-      status === STATUS_UNAUTHORIZED ||
-      status === STATUS_NOT_FOUND
-    ) {
-      auth.clearSession();
-      router.push({ name: "login" });
-      return;
+    const status = error?.response?.status;
+    if (status) {
+      if (status === 422) {
+        return Promise.reject({ validation: error.response.data });
+      }
+      if (
+        status === STATUS_BAD_REQUEST ||
+        status === STATUS_FORBIDDEN ||
+        status === STATUS_UNAUTHORIZED ||
+        status === STATUS_NOT_FOUND
+      ) {
+        const router = useRouter();
+        const auth = useAuthStore();
+        auth.clearSession();
+        router.push({ name: "login" });
+        return;
+      }
     }
     return Promise.reject({
-      message: "something went wrong, please try again later.",
+      message: error.message ?? "Something went wrong, please try again later",
     });
   },
 );
